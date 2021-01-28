87418a8708789544ff6afa9cf7d4e7c486d22d65
Simran <mdrsimran@gmail.com>
2021-01-28 14:05:03 -0600

    commit tree 68c403de908cb264ad2ec36368a0c86a4d6d8e04

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(strings, event){
+    let cards = []
+    for(let i=0; i <strings.length; i++){
+        cards.push(`Thank you, ${strings[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards
+}
+
+
+function countDown(num){
+    while(num > 0){
+        console.log(num);
+        num--;
+    }
+    console.log(num)
+}
